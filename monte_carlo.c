e73eaccefd4c3381de4a4ed9c0679bc69c9aa504
vams330 <85332016+vams330@users.noreply.github.com>
2021-06-04 11:37:42 +0530

    Update monte_carlo.c

--- monte_carlo.c
+++ monte_carlo.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <math.h>
 
-float mc_pi(int);
+
 
 float frandom() {
   long int q = random();
@@ -11,7 +11,21 @@ float frandom() {
   return ret;
 }
 
-int main(void) {
+float mc_pi(int n ){
+   int incircle = 0;
+   
+  for ( int i=0; i<=n ;i++ ){
+  float x = frandom();
+  float y= frandom();
+  float  d= x*x + y*y;
+  if ( d <= 1 ){
+   incircle++;
+   }
+  }
+  float pi = 4*(float)incircle/n;
+   return pi;   
+  }
+  intmain(void) {
   float pi0;
   float pi1;
   
